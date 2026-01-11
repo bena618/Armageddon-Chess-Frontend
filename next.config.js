c20dca144b126f19d90d34733b2257c3dec69013
bena618 <benaharon1@yahoo.com>
2026-01-10 22:22:54 -0500

    Had to fix a couple things in config file also did not have expected file name being .next.config instead next.config

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',             
+  trailingSlash: true,          
+  images: {
+    unoptimized: true,         
+  },
+   distDir: 'out',            
+   assetPrefix: '.',          
+};
+
+module.exports = nextConfig;
